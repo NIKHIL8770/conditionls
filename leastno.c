0c72f1a27c102e8d78cf8bab02ccc82b7542ecba
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 18:54:35 +0530

    Update leastno.c

--- leastno.c
+++ leastno.c
@@ -1,16 +1,17 @@
 
-  // #include<stdio.h>
-  // int main(){
-  // int a,b,c ;
-  //     printf("ENTER THE VALUER OF a    :",a);
-  //       scanf("%d",&a);
-  //  printf("ENTER THE VALUER OF b    :",b);
-  //   scanf("%d",&b);
-  //   printf("ENTER THE VALUER OF c    :",c);
-  //   scanf("%d",&c);
-  //   if(a>=b && a>=c)  printf("%d  is a greatest number\n",a);
-  //    if(b>=a && b>=c)  printf(" %d  is a greatest number\n",b);
-  //    if(c>=b && c>=a)  printf("%d  is a greatest number\n",c);
+   #include<stdio.h>
+   int main(){
+ int a,b,c ;
+      printf("ENTER THE VALUER OF a    :",a);
+        scanf("%d",&a);
+   printf("ENTER THE VALUER OF b    :",b);
+     scanf("%d",&b);
+     printf("ENTER THE VALUER OF c    :",c);
+     scanf("%d",&c);
+    if(a>=b && a>=c)  printf("%d  is a greatest number\n",a);
+     if(b>=a && b>=c)  printf(" %d  is a greatest number\n",b);
+    if(c>=b && c>=a)  printf("%d  is a greatest number\n",c);
 
-  //   return 0;
-  // }
+    return 0;
+  }
+  
