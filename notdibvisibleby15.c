c0249c7bffb188824f36ceaf4d9d15fd6de6357a
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 18:52:53 +0530

    Update notdibvisibleby15.c

--- notdibvisibleby15.c
+++ notdibvisibleby15.c
@@ -1,18 +1,18 @@
-// #include<stdio.h>
-// int main(){
-// int a ;
-//   printf("Enter a number   :");
-//   scanf("%d",&a);
-//   if(a%15!=0){
-//     if(a%3!=0){
-//       if(a%5==0) printf("condition valid") ;
-//       else
-//         printf(" condition invalid");
-//     }
-//     else 
-//       printf(" condition valid");
-//   }
-//   else 
-//     printf("not condition invalid");
-// return 0;
-// }
+#include<stdio.h>
+int main(){
+int a ;
+  printf("Enter a number   :");
+  scanf("%d",&a);
+  if(a%15!=0){
+    if(a%3!=0){
+      if(a%5==0) printf("condition valid") ;
+      else
+        printf(" condition invalid");
+    }
+    else 
+      printf(" condition valid");
+  }
+  else 
+    printf("not condition invalid");
+return 0;
+}
