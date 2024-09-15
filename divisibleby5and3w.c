d0fc350296bbfa09c050832c78d576636e8f83dc
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 19:09:50 +0530

    Update divisibleby5and3w.c

--- divisibleby5and3w.c
+++ divisibleby5and3w.c
@@ -1,12 +1,12 @@
-// #include<stdio.h>
-// int main(){
-// int a ;
-//   printf("Enter a number   :");
-//   scanf("%d",&a);
-//   if(a%5==0){
-//     if(a%3==0) printf("divisible by 5 and 3");
-//     else  printf("not divisible by 5 and 3");
-//   }
-//   else  printf("not divisible by 5 and 3");
-// return 0;
-// }
+#include<stdio.h>
+int main(){
+int a ;
+  printf("Enter a number   :");
+  scanf("%d",&a);
+  if(a%5==0){
+    if(a%3==0) printf("divisible by 5 and 3");
+    else  printf("not divisible by 5 and 3");
+  }
+  else  printf("not divisible by 5 and 3");
+return 0;
+}
