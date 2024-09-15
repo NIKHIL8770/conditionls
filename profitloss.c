adb7c83185e7ab06db0969b1ad839dc4166ca4c3
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 18:52:32 +0530

    Update profitloss.c

--- profitloss.c
+++ profitloss.c
@@ -1,16 +1,16 @@
-// #include<stdio.h>
-// int main(){
-// int sp,cp;
-//   printf("ENTER  sp :     ");
-//   scanf("%d",&sp);
-//   printf("ENTER  cp :     ");
-//   scanf("%d",&cp);
-//   if(cp>sp) printf("loss %d",cp-sp);
-//   else printf("profit");
-//   if(cp==sp) printf("nither profit nor loss%d",sp-cp);
+#include<stdio.h>
+int main(){
+int sp,cp;
+  printf("ENTER  sp :     ");
+  scanf("%d",&sp);
+  printf("ENTER  cp :     ");
+  scanf("%d",&cp);
+  if(cp>sp) printf("loss %d",cp-sp);
+  else printf("profit");
+  if(cp==sp) printf("nither profit nor loss%d",sp-cp);
 
-// return 0;
-// }
+return 0;
+}
 
 
 
