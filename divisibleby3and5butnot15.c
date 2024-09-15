647a1b0111d48b95dbbfbd42ac975363bb377ebb
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 19:10:04 +0530

    Update divisibleby3and5butnot15.c

--- divisibleby3and5butnot15.c
+++ divisibleby3and5butnot15.c
@@ -1,10 +1,10 @@
-// #include<stdio.h>
-// int main(){
-// int a ;
-//   printf("ENTERR A NUMBER     :");
-//   scanf("%d",&a);
-//   if(a%3==0 && a%5==0 && a%15!=0) printf("okay");
-//   else printf("not ok");
+#include<stdio.h>
+int main(){
+int a ;
+  printf("ENTERR A NUMBER     :");
+  scanf("%d",&a);
+  if(a%3==0 && a%5==0 && a%15!=0) printf("okay");
+  else printf("not ok");
   
-// return 0;
-// }
+return 0;
+}
